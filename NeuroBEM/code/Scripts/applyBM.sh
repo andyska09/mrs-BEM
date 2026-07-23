bf2871c05e5293d793978e11a489dd869067ac4b
Ondřej Skácel <skaceond@fit.cvut.cz>
2026-07-23 17:18:57 +0200

    config and folder name as script param

--- NeuroBEM/code/Scripts/applyBM.sh
+++ NeuroBEM/code/Scripts/applyBM.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-# ./applyBM.sh BASEPATH [filelist]
+# ./applyBM.sh BASEPATH MODEL CONFIG [filelist]
+#   MODEL    output subfolder name (e.g. a CMA-ES run timestamp)
+#   CONFIG   path to the params yaml (e.g. CMAES-results/<ts>/best.yaml)
 # the optional filelist argument can be the name of a
 # file that supplies a list of names to be converted
 
 programPath="../simulator/build/bem-model"
-model="2026-07-22-18-39-25"
-config="../../CMAES-results/2026-07-22-18-39-25/best.yaml"
 
-if [ "$1" != "" ]
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
 then
-  echo "Using Directory "$1
+  echo "usage: ./applyBM.sh BASEPATH MODEL CONFIG [filelist]"
+  exit 1
 fi
 
-mkdir -p "$1""$model"
+basepath="$1"
+model="$2"
+config="$3"
+filelist="$4"
 
-if [ -e "$2" ] 
+echo "Using Directory "$basepath
+echo "Model "$model" Config "$config
+
+mkdir -p "$basepath""$model"
+
+if [ -e "$filelist" ]
 then
   while read l
   do
     fname="merged_""$l"".csv"
-    dname=$1
     newfile=${fname:7}
-    echo "$dname" "$fname" "$newfile"
-    eval "$programPath" "$dname""$fname" "$dname""/""$model""/""$model""_""$newfile" "$config"
-  done < "$2"
+    echo "$basepath" "$fname" "$newfile"
+    eval "$programPath" "$basepath""$fname" "$basepath""/""$model""/""$model""_""$newfile" "$config"
+  done < "$filelist"
 else
-  for csv in "$1"merged_*seg*.csv
+  for csv in "$basepath"merged_*seg*.csv
   do
     fname=$(basename $csv)
     dname=$(dirname $csv)
     newfile=${fname:7}
-    # echo "$dname" "$fname" "$newfile"
     eval "$programPath" "$csv" "$dname""/""$model""/""$model""_""$newfile" "$config"
   done
 fi
-
