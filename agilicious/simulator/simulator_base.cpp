abdb22453c2bbca33059237df98ec5e8dd809920
Ondřej Skácel <skaceond@fit.cvut.cz>
2026-08-12 15:23:01 +0200

    remove agilicious

--- agilicious/simulator/simulator_base.cpp
+++
@@ -1,9 +0,0 @@
-#include "agilib/simulator/simulator_base.hpp"
-
-namespace agi {
-
-SimulatorBase::SimulatorBase(const std::string& name) : Module(name) {}
-
-SimulatorBase::~SimulatorBase() {}
-
-}  // namespace agi
