7152fb298a29663e9188abc24eca429179109157
Ondřej Skácel <skaceond@fit.cvut.cz>
2026-08-12 15:22:33 +0200

    clean up

--- NeuroBEM/code/Matlab/BEM/subroutines/calc_cd.m
+++
@@ -1,8 +0,0 @@
-function cd = calc_cd(alpha)
-%calc_cd returns the drag coefficient approximated by a three-term drag polar
-% cd = calc_cd(alpha) where
-%   alpha       angle of attack
-%   cd          drag coefficient
-    param = setParam();
-    cd = param.cd0 + param.cd1.*alpha + param.cd2.*alpha.^2;
-end
--- NeuroBEM/code/Matlab/BEM/subroutines/calc_v_hover.m
+++
@@ -1,7 +0,0 @@
-function v_hover = calc_v_hover(T)
-%calc_v_hover calculates the induced velocity at hover conditions given a
-% thrust T
-% v_hover = calc_v_hover(T)
-    param = setParam();
-    v_hover = sqrt(T/(2*param.rho*param.A));
-end
